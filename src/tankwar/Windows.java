f8e08246c61bc1b30520e560e333d875cf411407
kamiaki <247557768@qq.com>
2017-09-27 15:30:26 +0800

    控制坦克 动起来

--- src/tankwar/Windows.java
+++ src/tankwar/Windows.java
@@ -4,13 +4,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
 public class Windows extends JFrame implements InitValue{	
-	private int x = 50,xFangXiang = 1,xSpeed = 1,y = 50;	
+	private int x = 50,y = 50;	
 	
 	private mainPanel mPanel;
 	
@@ -20,11 +22,11 @@ public class Windows extends JFrame implements InitValue{
 	public Windows(){
 		launchFrame();
 	}
-	
 	/**
 	 * 初始化窗口
 	 */
 	public void launchFrame(){
+		//窗口生成
 		this.setTitle("Tank");
 		this.setSize(WindowsXlength, WindowsYlength);
 		this.setResizable(false);
@@ -35,16 +37,24 @@ public class Windows extends JFrame implements InitValue{
 			}		
 		});
 		
+		//键盘监听
+		this.addKeyListener(new Keylistener());
+		
+		//面板
 		mPanel = new mainPanel(); 
 		mPanel.setSize(WindowsXlength, WindowsYlength);
 		this.setContentPane(mPanel);
-		mPanel.setLayout(null);
-		
+		mPanel.setLayout(null);	
 		new Thread(new PaintThread()).start();
+		
 		
 		this.setVisible(true);
 	}
-	
+	/**
+	 * 重绘线程
+	 * @author Administrator
+	 *
+	 */
 	private class PaintThread implements Runnable{
 		@Override
 		public void run() {
@@ -52,48 +62,60 @@ public class Windows extends JFrame implements InitValue{
 			while(true){
 				mPanel.repaint();
 				try {
-					Thread.sleep(20);
+					Thread.sleep(10);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}	
 			}
 		}
 	}
-	
 	/*
 	 * 主窗口面板
 	 */
 	private class mainPanel extends JPanel{
-		
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);			
 			Image offScreenImage = Doublebuffer();
 			g.drawImage(offScreenImage, 0, 0 , null);
-			
-			xSpeed = x / 10;
-			if(xSpeed <= 0){
-				xSpeed = 1;
-			}
-			if(x > 600){
-				xFangXiang = -1;
-			}else if(x < 50){
-				xFangXiang = 1;
-			}
-			x = x + ( xFangXiang * xSpeed);	
-		}	
-		
+		}		
 		private Image Doublebuffer(){
 			Image image = mainPanel.this.createImage(WindowsXlength, WindowsYlength);
 			Graphics goffScreenImage = image.getGraphics();
 			goffScreenImage.setColor(Color.GREEN);
 			goffScreenImage.fillRect(0, 0, WindowsXlength, WindowsYlength);
 			goffScreenImage.setColor(Color.RED);
-			goffScreenImage.fillOval(x, y,30, 30);
-			
+			goffScreenImage.fillOval(x, y,30, 30);			
 			return image;
 		}
 	}
-	
-	
+	/**
+	 * 键盘监听类
+	 * @author Administrator
+	 *
+	 */
+	private class Keylistener extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			super.keyPressed(e);
+			int Key = e.getKeyCode();
+			switch (Key) {
+			case KeyEvent.VK_W:
+				y = y - 5;
+				break;
+			case KeyEvent.VK_S:
+				y = y + 5;
+				break;
+			case KeyEvent.VK_A:
+				x = x - 5;
+				break;
+			case KeyEvent.VK_D:
+				x = x + 5;
+				break;
+			default:
+				break;
+			}
+		}
+				
+		
+	}
 }
