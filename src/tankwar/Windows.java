07c59d587b086a84f46c22c15efb6c6d4c15e0d3
kamiaki <247557768@qq.com>
2017-09-27 13:19:33 +0800

    会动的坦克

--- src/tankwar/Windows.java
+++ src/tankwar/Windows.java
@@ -7,18 +7,11 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
-public class Windows extends JFrame{
-
-	private class mainPanel extends JPanel{
-		public void paint(Graphics g) {
-			super.paint(g);
-			Color c = g.getColor();
-			g.setColor(Color.RED);
-			g.fillOval(50, 50,30, 30);
-			g.setColor(c);
-		}		
-	}
+public class Windows extends JFrame{	
+	private mainPanel mPanel;
 	
+	private int x = 50, y = 50;
+
 	/**
 	 * 构造函数
 	 */
@@ -40,11 +33,42 @@ public class Windows extends JFrame{
 			}		
 		});
 		
-		mainPanel mPanel = new mainPanel(); 
+		mPanel = new mainPanel(); 
 		this.setContentPane(mPanel);
 		mPanel.setLayout(null);
 		mPanel.setBackground(Color.GREEN);
 		
+		new Thread(new PaintThread()).start();
+		
 		this.setVisible(true);
+	}
+	
+	private class PaintThread implements Runnable{
+		@Override
+		public void run() {
+			// TODO 自动生成的方法存根
+			while(true){
+				mPanel.repaint();
+				try {
+					Thread.sleep(10);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}	
+			}
+		}
+	}
+	
+	/*
+	 * 主窗口面板
+	 */
+	private class mainPanel extends JPanel{
+		public void paint(Graphics g) {
+			super.paint(g);
+			Color c = g.getColor();
+			g.setColor(Color.RED);
+			g.fillOval(x, y,30, 30);
+			g.setColor(c);	
+			x += 1;
+		}		
 	}
 }
